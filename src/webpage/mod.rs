fb83f66fdf0b8c3858bc77bd98aa2e0e4e65fde4
Chris Patuzzo <chris@patuzzo.co.uk>
2018-06-23 12:23:08 +0100

    Scale the canvas for retina displays

--- src/webpage/mod.rs
+++ src/webpage/mod.rs
@@ -56,12 +56,14 @@ impl Webpage {
 
     fn reset_styles(document: &web::Document, body: &web::HtmlElement) {
         let style = document.create_element("style").expect("failed to create element");
-        let css = document.create_text_node("
-            html, body { margin: 0; height: 100%; overflow: hidden; }
-        ");
+        let inner = document.create_text_node(Self::css_reset());
 
-        style.append_child(&css);
+        style.append_child(&inner);
         body.append_child(&style);
+    }
+
+    fn css_reset() -> &'static str {
+        "html, body, canvas { margin: 0; padding: 0; width: 100%; height: 100%; overflow: hidden; }"
     }
 
     fn add_canvas_to_page(body: &web::HtmlElement, canvas: &html_element::CanvasElement) {
@@ -70,9 +72,10 @@ impl Webpage {
 
     fn resize_canvas(body: &web::HtmlElement, canvas: &html_element::CanvasElement) {
         let rectangle = body.get_bounding_client_rect();
+        let pixel_ratio = Self::get_device_pixel_ratio();
 
-        canvas.set_width(rectangle.get_width() as u32);
-        canvas.set_height(rectangle.get_height() as u32);
+        canvas.set_width(rectangle.get_width() as u32 * pixel_ratio);
+        canvas.set_height(rectangle.get_height() as u32 * pixel_ratio);
     }
 
     fn bind_resize_event(window: &web::Window, body: &web::HtmlElement, canvas: &html_element::CanvasElement) {
@@ -82,5 +85,10 @@ impl Webpage {
         window.add_event_listener(move |_: event::ResizeEvent| {
             Self::resize_canvas(&b, &c)
         });
+    }
+
+    fn get_device_pixel_ratio() -> u32 {
+        let ratio = js! { return window.devicePixelRatio };
+        ratio.try_into().unwrap_or(1)
     }
 }
